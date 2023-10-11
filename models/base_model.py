8eea13590d1335f3ffb12356e36491e3b6d6cb84
ED-DEROUACH <oussama.ed-derouach@outlook.com>
2023-10-11 10:02:35 -0700

    up

--- models/base_model.py
+++ models/base_model.py
@@ -10,13 +10,30 @@ class BaseModel:
     The BaseModel class defines all common attributes/methods for other classes.
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Initializes a new instance of BaseModel.
+
+        Args:
+            *args: Not used.
+            **kwargs: Dictionary containing attribute names and values.
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key in ["created_at", "updated_at"]:
+                        value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, value)
+            if not hasattr(self, "id"):
+                self.id = str(uuid.uuid4())
+            if not hasattr(self, "created_at"):
+                self.created_at = datetime.now()
+            if not hasattr(self, "updated_at"):
+                self.updated_at = datetime.now()
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
