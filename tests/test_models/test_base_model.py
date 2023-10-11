91bb7c8e0f4ba6547b82c9c2f6e895bbe96f433b
ED-DEROUACH <oussama.ed-derouach@outlook.com>
2023-10-11 08:16:25 -0700

    up

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+This is a test script for the BaseModel class in the AirBnB project.
+"""
+import unittest
+from models.base_model import BaseModel
+
+class TestBaseModel(unittest.TestCase):
+    def test_attributes(self):
+        model = BaseModel()
+        self.assertTrue(hasattr(model, "id"))
+        self.assertTrue(hasattr(model, "created_at"))
+        self.assertTrue(hasattr(model, "updated_at"))
+
+    def test_str(self):
+        model = BaseModel()
+        string = model.__str__()
+        self.assertEqual(string, "[BaseModel] ({}) {}".format(model.id, model.__dict__))
+
+    def test_save(self):
+        model = BaseModel()
+        old_updated_at = model.updated_at
+        model.save()
+        self.assertNotEqual(old_updated_at, model.updated_at)
+
+    def test_to_dict(self):
+        model = BaseModel()
+        model_dict = model.to_dict()
+        self.assertTrue("__class__" in model_dict)
+        self.assertTrue("created_at" in model_dict)
+        self.assertTrue("updated_at" in model_dict)
+        self.assertEqual(model_dict["__class__"], "BaseModel")
+        self.assertEqual(model_dict["created_at"], model.created_at.isoformat())
+        self.assertEqual(model_dict["updated_at"], model.updated_at.isoformat())
+
+if __name__ == "__main__":
+    unittest.main()
+
