63bfc507d04e2c23b281b280770813ae1d59181a
ED-DEROUACH <97236123+ED-DEROUACH@users.noreply.github.com>
2023-10-20 15:27:53 +0100

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
-""" This module defines the review class """
-
-
+"""Defines the Review class."""
 from models.base_model import BaseModel
 
 
 class Review(BaseModel):
-    """ review class """
-    text = ""
+    """Represent a review.
+
+    Attributes:
+        place_id (str): The Place id.
+        user_id (str): The User id.
+        text (str): The text of the review.
+    """
+
     place_id = ""
     user_id = ""
+    text = ""
