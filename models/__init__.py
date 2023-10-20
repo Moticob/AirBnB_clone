86866e7e319edf104fcdef9ec328fb31ccbcaf57
ED-DEROUACH <97236123+ED-DEROUACH@users.noreply.github.com>
2023-10-20 15:23:51 +0100

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-"""
-This module initializes the models package.
-"""
+"""__init__ magic method for models directory"""
 from models.engine.file_storage import FileStorage
 
-# Create a unique FileStorage instance for your application
-storage = FileStorage()
 
-# Call the reload() method on this variable
+storage = FileStorage()
 storage.reload()
