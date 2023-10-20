765c89e154208e62e7fef88f853d3e4f99ebb56f
ED-DEROUACH <97236123+ED-DEROUACH@users.noreply.github.com>
2023-10-20 15:24:27 +0100

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-""" This module defines Amenity class """
-
-
+"""Defines the Amenity class."""
 from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
-    """ state class defined by name of the amenity """
+    """Represent an amenity.
+
+    Attributes:
+        name (str): The name of the amenity.
+    """
+
     name = ""
