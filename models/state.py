b381ade0505bdb0ab03b8267e7061d5a16f7241f
ED-DEROUACH <97236123+ED-DEROUACH@users.noreply.github.com>
2023-10-20 15:28:19 +0100

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-""" This module defines State class """
-
-
+"""Defines the State class."""
 from models.base_model import BaseModel
 
 
 class State(BaseModel):
-    """ state class defined by name of the state """
+    """Represent a state.
+
+    Attributes:
+        name (str): The name of the state.
+    """
+
     name = ""
